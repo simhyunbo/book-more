7a5e03829fe9879609986f3115c87ac863ebc617
simhyunbo <hbo9347@naver.com>
2023-01-17 17:30:20 +0900

    [Feat] repository 작성

--- backend/src/main/java/site/bookmore/bookmoreserver/alarms/Repository/AlarmRepository.java
+++ backend/src/main/java/site/bookmore/bookmoreserver/alarms/Repository/AlarmRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.bookmore.bookmoreserver.alarms.entity.Alarm;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+
 }
